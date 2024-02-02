f013b4b01aca8ca728360d5bc6b2ae00a1c69b25
bartoszpasiecznik <40137554+bartoszpasiecznik@users.noreply.github.com>
2024-02-02 13:20:39 +0100

    Sprawozdanie Lab 1-2

---
+++ 1-2/dBMeter_visualize.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plik z danymi 
+with open('./2024.01.31-15.41.20.dbmeter.txt') as f:
+  n_cols = len(f.readline().split("\t"))
+
+# moc sygnału w danym zakresie częstliwości dla każdej sekundy pomiaru
+freqData = np.loadtxt('./2024.01.31-15.41.20.dbmeter.txt', skiprows=1, usecols=np.arange(3,n_cols+1))
+# poziom głoności w całym paśmie dla każdej sekundy pomiaru
+loudnessData = np.loadtxt('./2024.01.31-15.41.20.dbmeter.txt', skiprows=1, usecols=2)
+# etykiety danych do wizualiacji
+labels = np.loadtxt('./2024.01.31-15.41.20.dbmeter.txt', dtype=str, max_rows=1, usecols=np.arange(2,n_cols))
+
+# uśrednienie mocy sygnału w poszczególnych pasmach za cały okres pomiaru
+meanFreqData = np.mean(freqData,axis=0)
+
+'''
+Przykładowe zobrazowanie widma i zmierzonych poziomów głośności dla średniej
+ze wszystkich pomiarów w czasie.
+'''
+plt.figure(figsize=(4,3),dpi=200)
+plt.plot(range(1,n_cols-1),meanFreqData)
+plt.bar(range(1,n_cols-1),meanFreqData)
+plt.xticks(range(1,n_cols-1), labels, rotation=90, fontsize=6)
+plt.yticks(fontsize=6)
+plt.title('Uśrednione widmo w pasmach oktawowych')
+
+plt.figure(figsize=(4,3),dpi=200)
+plt.plot(loudnessData)
+plt.title('Poziom głośności')
+
